8388990f17baa2eaa95d38f1744e17dd298b3459
liestiffani263 <liestiffani263@gmail.com>
2025-03-13 18:49:59 +0000

    Add 8.js

---
+++ src/8.js
@@ -0,0 +1,8 @@
+function getRandomJsCode() {
+  const code = `
+// School Project Code
+let name = prompt("What is your name?");
+alert("Hello " + name + ", nice to meet you!");
+`;
+return code;
+}
