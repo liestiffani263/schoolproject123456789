8bfacd0293c2016b85acc7091de6386d72fe5c39
liestiffani263 <liestiffani263@gmail.com>
2025-04-11 11:40:15 +0000

    Add 31.js

---
+++ src/31.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+const randomNumber = getRandomInt(100);
+console.log(`Random number between 0 and ${randomNumber}: ${randomNumber}`);
