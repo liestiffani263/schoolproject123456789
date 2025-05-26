229d9adb03f485df1066eaca563b946e4bcc79b3
liestiffani263 <liestiffani263@gmail.com>
2025-05-26 22:11:38 +0000

    Add 70.js

---
+++ src/70.js
@@ -0,0 +1,6 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Example usage: 
+console.log(getRandomInt(1, 100)); // Output a random integer between 1 and 100 (inclusive)
